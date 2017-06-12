a4b6ecdfb9b61a55ed1ab7cd601c77acbcb360ec
kato-masahiro <s1426030hj@s.chibakoudai.jp>
2017-06-12 09:19:38 +0000

    controller.pyとgo_around_with_controller.launchの編集
    launchファイルを編集して、必要なすべてのノードを一度に起動できるようにした
    controller.pyを編集して、コントローラで動かした時のマウスの速度を適切にした

--- launch/go_around_with_controller.py
+++
@@ -1,4 +0,0 @@
-<launch>
-  <include file="$(find raspimouse_ros_2)/launch/raspimouse.launch" /> 
-  <node pkg="raspimouse_maze" name="go_around" type="go_around_with_controller.py" required="true" output="screen" />
-</launch>
